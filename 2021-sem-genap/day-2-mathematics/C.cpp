01b5fe334b97b7ca916914427359e1fa2deba352
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-02-13 11:32:30 +0700

    change C

--- 2021-sem-genap/day-2-mathematics/C.cpp
+++ 2021-sem-genap/day-2-mathematics/C.cpp
@@ -1,51 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-bool is_prime[1000005];
-long long cnt[1000005], n, t;
+const int N = 1e6 + 2;
 
-void sieve(int n){
-	is_prime[0] = is_prime[1] = false;
-	for (int i = 2; i * i <= n; i++) {
-    	if (is_prime[i]) {
-        	for (int j = i * i; j <= n; j += i)
-            	is_prime[j] = false;
-    	}
-	}
-}
-bool takut(int n){
-	int now=n;
-	int cnt=0;
-	while(now>0){
-		if(now%10==0)return false;
-		else {
-			now/=10;
-			cnt++;
-		}
-	}
-	now=n;
-	int div=pow(10, cnt);
-	while(now>0){
-		now%=div;
-		div/=10;
-		if(now!=0 && is_prime[now]==false)return false;
-	}
-	return true;
-}
-void cnttakut(){
-	for(int i=2; i<=1000000; i++){
-		cnt[i]=cnt[i-1];
-		if(is_prime[i] && takut[i])cnt[i]++;
-	}
+int pre[N];
+bool not_prime[N];
+
+bool check(int x) {
+    vector<int> nums;
+    while (x > 0) {
+        nums.emplace_back(x % 10);
+        if (x % 10 == 0) return true;
+        x /= 10;
+    }
+    int cur = 1, res = 0;
+    for (int val : nums) {
+        res = val * cur + res;
+        if (not_prime[res]) return true;
+        cur = cur * 10;
+    }
+    return false;
 }
 
-int main(){
-	cin>>t;
-	for(int i=1; i<=1000000; i++)is_prime[i]=true;
-	sieve(1000000);
-	cnttakut();
-	for(int i=1; i<=t; i++){
-		cin>>n;
-		cout<<cnt[n]<<endl;
-	}
+void init() {
+    not_prime[1] = true;
+    for (int i = 2; i < N; i++) {
+        if (not_prime[i]) continue;
+        for (int j = i + i; j < N; j += i) {
+            not_prime[j] = true;
+        }
+    }
+    for (int i = 1; i < N; i++) {
+        pre[i] = pre[i - 1];
+        if (not_prime[i]) continue;
+        if (check(i)) continue;
+        pre[i]++;
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << pre[n] << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    init();
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
 }
