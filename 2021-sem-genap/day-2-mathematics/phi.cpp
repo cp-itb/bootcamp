499b7aaadfff2ea0a3993d3737de96cab72ead1a
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-02-13 09:38:17 +0700

    add phi.cpp

---
+++ 2021-sem-genap/day-2-mathematics/phi.cpp
@@ -0,0 +1,11 @@
+int phi(int n) {
+    int ret = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret -= ret / i;
+            while (n % i == 0) n /= i;
+        }
+    }
+    if (n > 1) ret -= ret / n;
+    return ret;
+}
